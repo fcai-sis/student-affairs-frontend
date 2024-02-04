31c68adafbfc5fa9c86678f897a16b0c3b7e1ecb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-02-04 18:16:51 +0200

    build students table page for registration session

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,15 +1,64 @@
-"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-import { useAtomValue } from "jotai";
-import registrationSessionAtom from "../students/register/registrationSessionAtom";
+import cancelFormAction from "./cancelAction";
+import confirmFormAction from "./confirmAction";
+import CancelSessionButton from "./cancelSessionButton";
+import ConfirmSessionButton from "./confirmSessionButton";
+import { readActiveRegistrationSession, readMappedStudents } from "../api";
 
-export default function TablePage() {
-  const registrationSession = useAtomValue(registrationSessionAtom);
+// TODO: Move to appropriate location
+export type Mapping = {
+  [key: string]: string,
+};
+
+export default async function Page() {
+  const activeRegistrationSession = await readActiveRegistrationSession();
+
+  if (!activeRegistrationSession) {
+    return redirect("/students/register");
+  }
+
+  const stagedStudents = await readMappedStudents(1);
 
   return (
-    <div>
-      <h1>Table Page</h1>
-      <p>Mapping: {JSON.stringify(registrationSession.mapping)}</p>
-    </div>
+    <>
+      <div className="table">
+        <div className="table-row">
+          {
+            Object.keys(activeRegistrationSession.mapping).map((column) => (
+              <div key={column} className="table-cell p-2 bg-gray-100">
+                {column}
+              </div>
+            ))
+          }
+        </div>
+        {
+          stagedStudents.map((student: any, index: number) => (
+            <div key={index} className="table-row">
+              {
+                Object.keys(activeRegistrationSession.mapping).map((column) => (
+                  <div key={column} className="table-cell p-2 bg-gray-50">
+                    {student[column]}
+                  </div>
+                ))
+              }
+            </div>
+          ))
+        }
+      </div>
+
+      <Link href="/students/register/mapping">
+        Update mapping
+      </Link>
+
+      <form action={cancelFormAction}>
+        <CancelSessionButton />
+      </form>
+
+      <form action={confirmFormAction}>
+        <ConfirmSessionButton />
+      </form>
+    </>
   );
 }

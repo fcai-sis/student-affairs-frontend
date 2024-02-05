d972e4b6fc6477ba6730db646b1c3fd4a17d0c3a
Amr Khaled <lewolfje@gmail.com>
2024-02-06 00:30:05 +0200

    Add UpdateButton ui component

---
+++ app/students/edit-student/update-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useFormStatus } from "react-dom";
+
+export function UpdateButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type='submit'
+      className={`bg-blue-500 text-white font-semibold py-2 px-4 mr-2 rounded transition-all ${
+        pending
+          ? "opacity-50 cursor-not-allowed "
+          : "hover:bg-blue-700 active:bg-blue-800"
+      }`}
+      aria-disabled={pending}
+    >
+      {pending ? "Updating..." : "Update"}
+    </button>
+  );
+}

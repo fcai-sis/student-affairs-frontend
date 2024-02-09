75c3125c06e6d22084c93246715ffd0b5d7813e6
Amr Khaled <lewolfje@gmail.com>
2024-02-09 10:25:53 +0200

    add function to count all students in db and return it in response

--- app/students/read-students/action.ts
+++ app/students/read-students/action.ts
@@ -1,6 +1,6 @@
 "use server";
 export async function readStudents(page: number) {
-  const searchParams = new URLSearchParams({ page: `${page}`, pageSize: "10" });
+  const searchParams = new URLSearchParams({ page: `${page}`, pageSize: "5" });
 
   const response = await fetch(
     `${process.env.STUDENT_REGISTRATION_API}/read?${searchParams}`,
@@ -13,13 +13,43 @@ export async function readStudents(page: number) {
     }
   );
 
-  const data = await response.json();
+  const studentData = await response.json();
+  const studentNum = await countStudents();
+
+  const students = studentData.students;
+  const totalStudents = studentNum.count;
 
   if (response.status === 200) {
-    return data.students;
+    return {
+      students,
+      totalStudents,
+    };
   } else if (response.status == 400) {
     console.log("Error: 400");
   }
 
   return [];
+}
+
+export async function countStudents() {
+  const response = await fetch(
+    `${process.env.STUDENT_REGISTRATION_API}/count`,
+    {
+      cache: "no-store",
+      method: "GET",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  if (response.status === 200) {
+    return data;
+  } else if (response.status == 400) {
+    console.log("Error: 400");
+  }
+
+  return 0;
 }

c447a54872f9a7df1787d5c2ac8215e9a1e0cfe6
Amr Khaled <lewolfje@gmail.com>
2024-02-10 11:58:24 +0200

    Refactor Navbar component and add internationalization support

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProfileHolder from "./ui/icons/ProfileHolder";
+import { getI18n } from "@/locales/server";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const t = await getI18n();
+
   return (
     <nav className='flex flex-wrap items-center gap-2 mb-2 mx-auto p-4 border-2 rounded-md border-b-blue-500'>
-      <Image src='/Logo.png' alt='logo' width={80} height={80} />
-      <div className='flex flex-grow gap-2'>
-        <Link href={"/services"}>Services</Link>
-        <Link href={"/announcements"}>Announcements</Link>
-        <Link href={"/students/read-students"}>Students</Link>
-        <Link href={"/"}>Home</Link>
-      </div>
       <ProfileHolder />
+      <div className='flex flex-grow gap-2'>
+        <Link
+          className='transition duration-300 ease-in-out transform hover:scale-110 hover:bg-gray-200 px-4 py-2 rounded-md active:bg-gray-400'
+          href={"/"}
+        >
+          {t("home")}
+        </Link>
+        <Link
+          className='transition duration-300 ease-in-out transform hover:scale-110 hover:bg-gray-200 px-4 py-2 rounded-md active:bg-gray-400'
+          href={"/students/read-students"}
+        >
+          {t("students")}
+        </Link>
+        <Link
+          className='transition duration-300 ease-in-out transform hover:scale-110 hover:bg-gray-200 px-4 py-2 rounded-md active:bg-gray-400'
+          href={"/announcements"}
+        >
+          {t("announcements")}
+        </Link>
+        <Link
+          className='transition duration-300 ease-in-out transform hover:scale-110 hover:bg-gray-200 px-4 py-2 rounded-md active:bg-gray-400
+'
+          href={"/services"}
+        >
+          {t("services")}
+        </Link>
+      </div>
+      <Image src='/Logo.png' alt='logo' width={80} height={80} />
     </nav>
   );
 };

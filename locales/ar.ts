a117a594bfd1494d4ce6e44cd97c8e90030065cb
Amr Khaled <lewolfje@gmail.com>
2024-02-10 11:59:01 +0200

    Update localization for Arabic and English

--- locales/ar.ts
+++ locales/ar.ts
@@ -1,6 +1,16 @@
 // locales/ar.ts
 export default {
-  hello: "هلا",
-  "hello.world": "!هلا يا عالم",
+  home: "الصفحة الرئيسية",
+  students: "الطلاب",
+  announcements: "الإعلانات",
+  services: "الخدمات",
+  "Register Students": "تسجيل الطلاب",
+  "Manual Registration": "تسجيل يدوي",
+  "Add a student manually": "أضف طالب يدوياً",
+  "Upload Excel": "رفع ملف اكسل",
+  "Upload an excel file with student information":
+    "قم برفع ملف اكسل يحتوي على معلومات الطلاب",
+  "Please ensure that the student information is correct before uploading":
+    "يرجى التأكد من صحة معلومات الطلاب قبل الرفع",
   welcome: "مرحبا {name}!",
 } as const;
--- locales/en.ts
+++ locales/en.ts
@@ -1,6 +1,16 @@
 // locales/en.ts
 export default {
-  hello: "Hello",
-  "hello.world": "Hello world!",
+  home: "Home",
+  students: "Students",
+  announcements: "Announcements",
+  services: "Services",
+  "Register Students": "Register Students",
+  "Manual Registration": "Manual Registration",
+  "Add a student manually": "Add a student manually",
+  "Upload Excel": "Upload Excel",
+  "Upload an excel file with student information":
+    "Upload an excel file with student information",
+  "Please ensure that the student information is correct before uploading":
+    "Please ensure that the student information is correct before uploading",
   welcome: "Hello {name}!",
 } as const;

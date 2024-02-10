d3933bca76992ed8a80d8d4dce33bb78605ba5ab
Amr Khaled <lewolfje@gmail.com>
2024-02-10 18:09:33 +0200

    Add ChangeLanguageButton component and icons

--- locales/client.ts
+++ locales/client.ts
@@ -1,9 +1,7 @@
-// locales/client.ts
-"use client";
-import { createI18nClient } from "next-international/client";
+"use client"
+import { createI18nClient } from 'next-international/client'
 
-export const { useI18n, useScopedI18n, I18nProviderClient } = createI18nClient({
-  en: () => import("./en"),
-  ar: () => import("./ar"),
-});
-
+export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale, } = createI18nClient({
+  en: () => import('./en'),
+  ar: () => import('./ar')
+})

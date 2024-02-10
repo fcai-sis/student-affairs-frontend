eb4e78a7cf3d52246994a8c7f1bc423be737abc2
Amr Khaled <lewolfje@gmail.com>
2024-02-10 11:58:06 +0200

    Remove unused imports and variables

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,7 @@
 import Navbar from "@/components/Navbar";
 import RegisterButtons from "@/components/RegisterButtons";
-import { getI18n, getScopedI18n } from "@/locales/server";
 
 export default async function Home() {
-  const t = await getI18n();
-  const scopedT = await getScopedI18n("hello");
   return (
     <div>
       <Navbar />

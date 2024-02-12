10231d9b0bfe01b736c05015dd7223c23aeec733
Amr Khaled <lewolfje@gmail.com>
2024-02-12 20:34:59 +0200

    refactor schema, min(1) doesn't work with arabic

--- app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/types.ts
+++ app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/types.ts
@@ -1,15 +1,16 @@
 import { z } from "zod";
+
 export const mappingSchema = z
   .object({
-    fullName: z.string().min(1),
-    address: z.string().min(1),
-    status: z.string().min(1),
-    studentId: z.string().min(1),
+    fullName: z.string(),
+    address: z.string(),
+    status: z.string(),
+    studentId: z.string(),
   })
   .refine(
     (data) => {
       // Check if any of the fields have the value '<unset>'
-      return Object.values(data).some((value) => value === "<unset>");
+      return !Object.values(data).some((value) => value === "<unset>");
     },
     {
       message: 'Fields cannot be "<unset>"',

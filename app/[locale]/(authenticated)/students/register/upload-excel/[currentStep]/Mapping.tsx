8f8ebcdfaff0dace54f191f476c6e2fb887965e4
Amr Khaled <lewolfje@gmail.com>
2024-02-12 20:10:22 +0200

    WIP mapping

--- app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/Mapping.tsx
+++ app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/Mapping.tsx
@@ -1,7 +1,16 @@
-export default function Mapping() {
+import { readActiveRegistrationSession } from "@/app/api";
+import MapForm from "./MapForm";
+
+export default async function Mapping() {
+  const activeRegistrationSession = await readActiveRegistrationSession();
+  if (!activeRegistrationSession)
+    return <div>No active registration session</div>;
+
   return (
-    <>
-      mapping
-    </>
+    <div>
+      <h1>
+        <MapForm Headers={activeRegistrationSession.excelColumnsHeaders} Mapping={activeRegistrationSession.mapping}/>
+      </h1>
+    </div>
   );
 }

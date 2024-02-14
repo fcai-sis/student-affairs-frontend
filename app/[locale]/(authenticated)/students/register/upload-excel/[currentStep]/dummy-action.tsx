be0b5937fb0abed5012eeda44131e8cd752d3f99
Amr Khaled <lewolfje@gmail.com>
2024-02-14 20:46:24 +0200

    dummy till we get the endpoint to validate

---
+++ app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/dummy-action.tsx
@@ -0,0 +1,25 @@
+"use server";
+export default async function dummyEndpoint(delay: number): Promise<boolean> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, delay);
+  });
+}
+
+export async function commitSession() {
+  const response = await fetch(
+    `${process.env.STUDENT_REGISTRATION_API}/commit`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+
+    // coolcool just send the data or the error if one occurs
+  return null;
+}

8ed422e68f59c867c535ebad7827a99bbdb84d39
Amr Khaled <lewolfje@gmail.com>
2024-02-15 15:20:44 +0200

    i m not good at this

--- app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/MapForm.tsx
+++ app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/MapForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useFormState } from "react-dom";
 import validateMapping from "./map-action";
+import Button from "@/components/Button";
+import { LongArrowUpRightIcon, NavArrowLeftIcon } from "@/components/icons";
 
 export default function MapForm({
   headers,
@@ -15,32 +17,61 @@ export default function MapForm({
   const [state, formAction] = useFormState(validateMapping, null);
 
   return (
-    <div>
-      <h1>MapForm</h1>
-      <ul>
+    <>
+      <div>
+        <p className='flex w-[387] h-min text-slate-400 text-sm '>
+          من فضلك قم بربط الأعمدة الصحيحة في ملف الإكسل بالأعمدة المقابلة لها في
+          قاعدة البيانات
+        </p>
+      </div>
+      <div>
         <form action={formAction}>
           {mappingKeys.map((field, index) => (
-            <li key={index}>
-              <label htmlFor={field}>{field}</label>
-              <select
-                required={true}
-                name={field}
-                defaultValue={mapping[field]}
-              >
-                <option disabled={true} value='<unset>'>
-                  Select the column that this field represents
-                </option>
-                {headers.map((header, index) => (
-                  <option key={index} value={header}>
-                    {header}
+            <div
+              className='w-min h-[100px] flex justify-center gap-8 gap-y-16'
+              key={index}
+            >
+              <div className='w-[142px] h-max flex flex-col items-start'>
+                <label className='w-max h-max flex ' htmlFor={field}>
+                  {field}
+                </label>
+              </div>
+              <div className='w-[142px] h-max flex flex-col items-start'>
+                <select
+                  className='flex flex-col w-max h-max justify-end'
+                  required={true}
+                  name={field}
+                  defaultValue={mapping[field]}
+                >
+                  <option disabled={true} value='<unset>'>
+                    Select the column that this field represents
                   </option>
-                ))}
-              </select>
-            </li>
+                  {headers.map((header, index) => (
+                    <option key={index} value={header}>
+                      {header}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
           ))}
-          <button type='submit'>Submit</button>
+          <div className='w-max h-min flex justify-center gap-4'>
+            <Button
+              variant='secondary'
+              icon={<LongArrowUpRightIcon className='stroke-slate-400' />}
+            >
+              Back
+            </Button>
+            <Button
+              variant='primary'
+              icon={<NavArrowLeftIcon className='stroke-slate-50' />}
+              type='submit'
+            >
+              Submit
+            </Button>
+          </div>
         </form>
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }

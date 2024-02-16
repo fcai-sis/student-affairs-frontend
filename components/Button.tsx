0f0094612203a8adec57504fdce295332e1aabfd
Amr Khaled <lewolfje@gmail.com>
2024-02-16 15:11:12 +0200

    Update Button component styles

--- components/Button.tsx
+++ components/Button.tsx
@@ -24,7 +24,7 @@ export function buttonClassName(variant: "primary" | "danger" | "secondary") {
     primary: "bg-blue-600 text-slate-50 hover:bg-blue-500 active:bg-blue-700",
     danger: "bg-red-600 text-slate-50 hover:bg-red-500 active:bg-red-700",
     secondary:
-      "bg-slate-100 text-slate-400 hover:bg-slate-200 active:bg-slate-300",
+      "bg-slate-50 text-slate-400 hover:bg-slate-200 active:bg-slate-300",
   };
 
   const className = `cursor-pointer flex justify-center gap-2 p-4 rounded-lg ${style[variant]} transition-colors duration-200 shadow-md hover:shadow-lg active:shadow-none outline-none w-fit min-w-28 font-bold whitespace-nowrap disabled:bg-slate-300 disabled:text-slate-400 disabled:cursor-not-allowed disabled:hover:bg-slate-300 disabled:hover:text-slate-400 disabled:active:bg-slate-300 disabled:active:text-slate-400 disabled:shadow-none disabled:outline-none disabled:shadow-none disabled:hover:shadow-none disabled:active:shadow-none disabled:outline-none`;
@@ -65,8 +65,6 @@ export default function Button({
     </button>
   );
 }
-
-
 
 export function SubmitButton() {
   const { pending } = useFormStatus();

02051999069e3655d2e57cccf3f0f9f42b0e9e61
Amr Khaled <lewolfje@gmail.com>
2024-05-05 18:34:16 +0300

    add components

---
+++ components/ServiceRequestCard.tsx
@@ -0,0 +1,10 @@
+export default function ServiceRequestCard() {
+  return (
+    <div className='bg-white shadow-md p-4 rounded-lg'>
+      <h1>Service</h1>
+      <div>
+        <p>Request</p>
+      </div>
+    </div>
+  );
+}

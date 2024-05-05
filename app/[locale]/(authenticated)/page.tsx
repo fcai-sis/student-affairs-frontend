d59b89c9a249d685447473e8d453433204470835
Amr Khaled <lewolfje@gmail.com>
2024-05-05 18:37:17 +0300

    hoh

--- app/[locale]/(authenticated)/page.tsx
+++ app/[locale]/(authenticated)/page.tsx
@@ -1,9 +1,14 @@
-export default async function Page({ params: { locale } }: { params: { locale: string } }) {
-  const name = locale === 'en' ? 'John' : 'جون';
+export default async function Page({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const name = locale === "en" ? "John" : "جون";
 
   return (
     <div>
       <h1>{name}</h1>
+      <h1>HOME</h1>
     </div>
-  )
+  );
 }

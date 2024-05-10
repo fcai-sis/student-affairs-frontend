d432394e5898760297f50104eed8588c5c0e202c
Amr Khaled <lewolfje@gmail.com>
2024-05-10 21:51:08 +0300

    refactor api call

--- app/[locale]/students/add-student/api.ts
+++ app/[locale]/students/add-student/api.ts
@@ -1,30 +1,24 @@
 import { TODO } from "../TODO";
 export async function addStudent(student: TODO) {
-  const response = await fetch(
-    `${process.env.STUDENT_REGISTRATION_API}/create`,
-    {
-      method: "POST",
-      body: JSON.stringify(student.data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await fetch(`http://127.0.0.1:3001/students/create`, {
+    method: "POST",
+    body: JSON.stringify(student.data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   const data = await response.json();
 
-  // Check if the response has an 'error' property
-  if (data.error) {
-    if (data.error.message) {
-      console.error("Error:", data.error.message);
-      return { error: data.error.message };
-    }
-    // Handle error case
-    const { error } = data;
-    console.error("Error:", error);
-    return { error };
-  } else {
-    const { student } = data;
-    return { student };
+  if (response.status === 201) {
+    return data.student;
+  } else if (response.status === 400) {
+    return {
+      error: data.error,
+    };
   }
+
+  return {
+    error: "An error occurred",
+  };
 }

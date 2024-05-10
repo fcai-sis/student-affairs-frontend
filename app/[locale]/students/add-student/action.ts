35d8a0d7eb6d8ef46656f8a4add7ef8d265e0215
Amr Khaled <lewolfje@gmail.com>
2024-05-10 21:50:42 +0300

    add all student fields

--- app/[locale]/students/add-student/action.ts
+++ app/[locale]/students/add-student/action.ts
@@ -4,10 +4,28 @@ import { revalidatePath } from "next/cache";
 import { StudentSchema } from "../lib/types";
 import { addStudent } from "./api";
 export async function createStudent(_: any, formData: FormData) {
+  const birthDateValue = formData.get("birthDate") as string;
+  // split the birth date value into year, month, and day
+  const [birthYear, birthMonth, birthDay] = birthDateValue.split("-");
+
   const rawFormData = {
-    fullName: formData.get("student-name"),
-    status: formData.get("student-status"),
-    address: formData.get("student-address"),
+    fullName: formData.get("fullName") as string,
+    studentId: formData.get("studentId") as string,
+    groupCode: (formData.get("groupCode") as string) === "1",
+    gender: formData.get("gender") as string,
+    religion: formData.get("religion") as string,
+    nationalId: formData.get("nationalId") as string,
+    administration: formData.get("administration") as string,
+    directorate: formData.get("directorate") as string,
+    phoneNumber: formData.get("phoneNumber") as string,
+    educationType: formData.get("educationType") as string,
+    birthYear: parseInt(birthYear),
+    birthMonth: parseInt(birthMonth),
+    birthDay: parseInt(birthDay),
+    birthPlace: formData.get("birthPlace") as string,
+    governorateId: parseInt(formData.get("governorateId") as string),
+    nationality: formData.get("nationality") as string,
+    address: formData.get("address") as string,
   };
 
   const parsedStudent = StudentSchema.safeParse(rawFormData);
@@ -22,7 +40,17 @@ export async function createStudent(_: any, formData: FormData) {
       error: errorMessage,
     };
   }
+
+  console.log(parsedStudent.data);
+
   const response = await addStudent(parsedStudent);
-  revalidatePath("/students/read-students");
+
+  if (response.error) {
+    return {
+      error: response.error.message,
+    };
+  }
+
+  // revalidatePath("/students/read-students");
   return response;
 }
--- app/[locale]/students/read-students/page.tsx
+++ app/[locale]/students/read-students/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
   let page = parseInt(searchParams.page, 10);
   if (!page || page < 1) page = 1;
 
-  const data : TODO = await readStudents(page);
+  const data: TODO = await readStudents(page);
 
   const totalPages = Math.ceil(data.totalStudents / PAGE_SIZE);
   if (data.students.length === 0) {
@@ -31,7 +31,19 @@ export default async function Page({
         <thead>
           <tr className='bg-gray-200'>
             <th className='py-2 px-4 border-r'>Full Name</th>
-            <th className='py-2 px-4 border-r'>Status</th>
+            <th className='py-2 px-4 border-r'>Student ID</th>
+            <th className='py-2 px-4 border-r'>Group Code</th>
+            <th className='py-2 px-4 border-r'>Gender</th>
+            <th className='py-2 px-4 border-r'>Religion</th>
+            <th className='py-2 px-4 border-r'>National ID</th>
+            <th className='py-2 px-4 border-r'>Administration</th>
+            <th className='py-2 px-4 border-r'>Directorate</th>
+            <th className='py-2 px-4 border-r'>Phone Number</th>
+            <th className='py-2 px-4 border-r'>Education Type</th>
+            <th className='py-2 px-4 border-r'>Birth Date</th>
+            <th className='py-2 px-4 border-r'>Birth Place</th>
+            <th className='py-2 px-4'>Governorate ID</th>
+            <th className='py-2 px-4'>Nationality</th>
             <th className='py-2 px-4'>Address</th>
             <th className='py-2 px-4'>Actions</th>
           </tr>
@@ -40,13 +52,24 @@ export default async function Page({
           {students.map((student: any, index: number) => (
             <tr key={index}>
               <td className='py-2 px-4 border-r'>{student.fullName}</td>
-              <td className='py-2 px-4 border-r'>{student.status}</td>
+              <td className='py-2 px-4 border-r'>{student.studentId}</td>
+              <td className='py-2 px-4 border-r'>{
+                student.groupCode === 0 ? 'Science Group' : 'Math Group'
+              }</td>
+              <td className='py-2 px-4 border-r'>{student.gender}</td>
+              <td className='py-2 px-4 border-r'>{student.religion}</td>
+              <td className='py-2 px-4 border-r'>{student.nationalId}</td>
+              <td className='py-2 px-4 border-r'>{student.administration}</td>
+              <td className='py-2 px-4 border-r'>{student.directorate}</td>
+              <td className='py-2 px-4 border-r'>{student.phoneNumber}</td>
+              <td className='py-2 px-4 border-r'>{student.educationType}</td>
+              <td className='py-2 px-4 border-r'>{student.birthDate}</td>
+              <td className='py-2 px-4 border-r'>{student.birthPlace}</td>
+              <td className='py-2 px-4 border-r'>{student.governorateId}</td>
+              <td className='py-2 px-4 border-r'>{student.nationality}</td>
               <td className='py-2 px-4'>{student.address}</td>
               <td className='py-2 px-4 flex items-center justify-center space-x-2'>
-                <Link
-                  className='text-white-500'
-                  href={`/students/edit-student/id=${student._id}`}
-                >
+                <Link href={`/students/edit-student/id=${student._id}`}>
                   <EditBtn />
                 </Link>
                 <DeleteStudentForm id={student._id} />

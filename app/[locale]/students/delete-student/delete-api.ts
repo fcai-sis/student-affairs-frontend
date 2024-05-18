2e0f83af52545f7ae0c5cfcddf5b5ece0492256f
Amr Khaled <lewolfje@gmail.com>
2024-05-18 22:45:22 +0300

    delete works

--- app/[locale]/students/delete-student/delete-api.ts
+++ app/[locale]/students/delete-student/delete-api.ts
@@ -3,15 +3,12 @@ import { revalidatePath } from "next/cache";
 import { TODO } from "../TODO";
 
 export default async function deleteStudent(id: string) {
-  const res = await fetch(
-    `${process.env.STUDENT_REGISTRATION_API}/delete/${id}`,
-    {
-      method: "DELETE",
-    }
-  );
+  const res = await fetch(`http://127.0.0.1:3001/students/delete/${id}`, {
+    method: "DELETE",
+  });
 
   const data: TODO = await res.json();
-  console.log(data);
+  console.log("DELETED STUDENT DATA", data);
 
   if (res.status === 200) {
     revalidatePath("/students/read-students");

7f3331855f9916b97adccbc980d06e949890515e
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-15 11:09:53 +0300

    reformat

--- app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/(1)/UploadExcelForm.tsx
+++ app/[locale]/(authenticated)/students/register/upload-excel/[currentStep]/(1)/UploadExcelForm.tsx
@@ -5,9 +5,14 @@ import { useFormState } from "react-dom";
 
 import { P } from "@/components/H";
 import Button, { buttonClassName } from "@/components/Button";
-import { CheckCircleSolidIcon, NavArrowLeftIcon, UploadIcon, XmarkCircleSolidIcon } from "@/components/icons";
+import {
+  CheckCircleSolidIcon,
+  NavArrowLeftIcon,
+  UploadIcon,
+  XmarkCircleSolidIcon,
+} from "@/components/icons";
 import uploadFileAction from "./action";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export type UploadExcelFormState = {
   uploaded: boolean;
@@ -21,22 +26,20 @@ export default function UploadExcelForm() {
   const router = useRouter();
 
   const [file, setFile] = useState<string | null>(null);
-  const [state, formAction] = useFormState(uploadFileAction, { uploaded: false });
+  const [state, formAction] = useFormState(uploadFileAction, {
+    uploaded: false,
+  });
 
   useEffect(() => {
-    if (state.uploaded)
-      router.push("/students/register/upload-excel/2")
-  }, [state])
+    if (state.uploaded) router.push("/students/register/upload-excel/2");
+  }, [state]);
 
   return (
     <form
       className="w-min h-min flex flex-col items-center gap-8"
       action={formAction}
     >
-      <label
-        className={buttonClassName("secondary")}
-        htmlFor="file"
-      >
+      <label className={buttonClassName("secondary")} htmlFor="file">
         {"اختر ملف"}
         <UploadIcon className="stroke-slate-400" />
       </label>
@@ -54,41 +57,31 @@ export default function UploadExcelForm() {
         }}
       />
       <div className="flex gap-2 justify-center">
-        {
-          file ? (
-            <P className="text-slate-500 whitespace-nowrap">
-              {file}
-            </P>
-          )
-            : (
-              <P className="text-slate-400 font-bold whitespace-nowrap">
-                {"لم تقم باختيار ملف بعد"}
-              </P>
-            )
-        }
-        {
-          file
-            ? (<CheckCircleSolidIcon className="fill-green-500" />)
-            : (<XmarkCircleSolidIcon className="fill-red-500" />)
-        }
-      </div>
-      {
-        state.error &&
-        (
-          <P className="font-bold text-red-500 w-96 text-center">
-            {state.error}
+        {file ? (
+          <P className="text-slate-500 whitespace-nowrap">{file}</P>
+        ) : (
+          <P className="text-slate-400 font-bold whitespace-nowrap">
+            {"لم تقم باختيار ملف بعد"}
           </P>
-        )
-      }
+        )}
+        {file ? (
+          <CheckCircleSolidIcon className="fill-green-500" />
+        ) : (
+          <XmarkCircleSolidIcon className="fill-red-500" />
+        )}
+      </div>
+      {state.error && (
+        <P className="font-bold text-red-500 w-96 text-center">{state.error}</P>
+      )}
       <div className="flex gap-2">
         <Button
           type="submit"
           variant="primary"
-          icon={<NavArrowLeftIcon className="stroke-slate-50" />}>
+          icon={<NavArrowLeftIcon className="stroke-slate-50" />}
+        >
           {continueButtonText}
         </Button>
       </div>
-    </form >
+    </form>
   );
 }
-

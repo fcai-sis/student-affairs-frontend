c1ee4313d9d44c863008df5784a4138be47c95fb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-06-18 12:00:39 +0300

    refactor: Update AnnouncementCard layout and add academicLevel and department information
    This commit updates the layout of the AnnouncementCard component, increasing its width to 72 units. It also adds the academicLevel and department information to the card, displaying them when available. The academicLevel is shown as a bold text, and the department is displayed as a list of names. If no department is specified, "All" is shown instead. This change improves the display of announcement details and provides more context to the users.

--- components/AnnouncementCard.tsx
+++ components/AnnouncementCard.tsx
@@ -1,3 +1,5 @@
+import { CreatedAt } from "./CreatedAt";
+
 export default async function AnnouncementCard({
   announcement,
 }: {
@@ -10,9 +12,21 @@ export default async function AnnouncementCard({
       ? "bg-yellow-100"
       : "bg-red-100";
   return (
-    <div className={`border border-black w-64 p-4 ${severityColor}`}>
+    <div className={`border border-black w-72 p-4 ${severityColor}`}>
       <h3>{announcement.title}</h3>
       <p>{announcement.content}</p>
+      <p>
+        Level: <b>{announcement.academicLevel ?? "All"}</b>
+      </p>
+      Department:{" "}
+      <b>
+        {announcement.department.length > 0
+          ? announcement.department.map((department: any, i: number) => (
+              <p key={i}>{department.name.en}</p>
+            ))
+          : "All"}
+      </b>
+      <CreatedAt date={announcement.createdAt} />
     </div>
   );
 }

b844d9a53c9d9fe3a9760526abc6dcbd8fb3b3ec
Amr Khaled <lewolfje@gmail.com>
2024-07-02 23:36:57 +0300

    refactor these stupid ass cards im tired

--- components/ServiceRequestCard.tsx
+++ components/ServiceRequestCard.tsx
@@ -1,24 +1,66 @@
+import { getCurrentLocale } from "@/locales/server";
 import { CreatedAt } from "./CreatedAt";
 import ServiceRequestCardImage from "./ServiceRequestCardImage";
 import Link from "next/link";
+import {
+  ServiceRequestStatusEnum,
+  ServiceRequestStatusEnumType,
+  serviceRequestStatusLocalizedFields,
+} from "@fcai-sis/shared-models";
+import { tt } from "@/lib";
 
 export default async function ServiceRequestCard({
   serviceRequest,
 }: {
   serviceRequest: any;
 }) {
+  const locale = getCurrentLocale();
+  const colors =
+    serviceRequest.status === ServiceRequestStatusEnum[0]
+      ? "bg-yellow-100 text-yellow-500"
+      : serviceRequest.status === ServiceRequestStatusEnum[1]
+      ? "bg-blue-100 text-blue-500"
+      : serviceRequest.status === ServiceRequestStatusEnum[2]
+      ? "bg-green-100 text-green-500"
+      : "bg-red-100 text-red-500";
   return (
-    <div className={`border border-black w-64 p-4`}>
-      <h3>{serviceRequest.serviceName}</h3>
-      <p>{serviceRequest.message}</p>
-      <p>
-        Status: <b>{serviceRequest.status}</b>
-      </p>
-      <p>{serviceRequest.student.fullName}</p>
-      <p>{serviceRequest.student.studentId}</p>
-      <ServiceRequestCardImage src={serviceRequest.image} />
-      <Link href={`/requests/${serviceRequest._id}`}>View details</Link>
-      <CreatedAt date={serviceRequest.createdAt} />
+    <div className={`border border-slate-200 w-full p-4 rounded-lg my-2`}>
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-2'>
+          <div className='flex flex-col gap-2'>
+            <small
+              className={`${colors} flex justify-center rounded-lg px-2 py-1 h-min`}
+            >
+              {tt(
+                locale,
+                serviceRequestStatusLocalizedFields[
+                  serviceRequest.status as ServiceRequestStatusEnumType
+                ]
+              )}
+            </small>
+            {/* <p className='text-xl'>
+              {tt(locale, {
+                en: "Service Request",
+                ar: "طلب خدمة",
+              })}
+            </p> */}
+            <h5 className=''>{serviceRequest.serviceName}</h5>
+            {/* <p className='text-xl'>
+              {tt(locale, {
+                en: "Requested by",
+                ar: "طلب بواسطة",
+              })}
+            </p> */}
+            <div className=''>
+              <p>{serviceRequest.student.fullName}</p>
+              <p>{serviceRequest.student.studentId}</p>
+            </div>
+            <ServiceRequestCardImage src={serviceRequest.image} />
+            <Link href={`/requests/${serviceRequest._id}`}>View details</Link>
+            <CreatedAt date={serviceRequest.createdAt} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- components/ServiceRequestCardImage.tsx
+++ components/ServiceRequestCardImage.tsx
@@ -10,21 +10,23 @@ export default function ServiceRequestCardImage({ src }: { src: string }) {
     <>
       <Image
         src={src}
-        alt="Service request invoice image"
+        alt='Service request invoice image'
         width={80}
         height={80}
         onClick={(_) => setIsModalOpen(true)}
+        className='cursor-pointer rounded-lg brightness-75 hover:brightness-105 transition-all duration-300'
       />
       <div
-        className="w-full h-full fixed top-0 left-0 bg-black bg-opacity-50 flex justify-center items-center"
+        className='z-50 w-full h-full fixed top-0 left-0 bg-black bg-opacity-65'
         style={{ display: isModalOpen ? "block" : "none" }}
+        onClick={(_) => setIsModalOpen(false)}
       >
         <Image
           src={src}
-          alt="Service request invoice image"
-          width={400}
-          height={400}
-          onClick={(_) => setIsModalOpen(false)}
+          alt='Service request invoice image'
+          width={500}
+          height={500}
+          className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
         />
       </div>
     </>

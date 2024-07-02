74bb2de465b3d7507e677669cdeb4d404878f18e
Amr Khaled <lewolfje@gmail.com>
2024-07-02 17:14:09 +0300

    add apis

--- api.ts
+++ api.ts
@@ -24,11 +24,6 @@ export const coursesAPI = axios.create({
   baseURL: process.env.COURSES_API_URL,
   validateStatus: (status) => status < 500,
 });
-
-export const departmentsAPI = axios.create({
-  baseURL: process.env.DEPARTMENTS_API_URL,
-  validateStatus: (status) => status < 500,
-});
 export const enrollmentsAPI = axios.create({
   baseURL: process.env.ENROLLMENTS_API_URL,
   validateStatus: (status) => status < 500,
@@ -39,7 +34,42 @@ export const profileAPI = axios.create({
   validateStatus: (status) => status < 500,
 });
 
-export const graduationAPI = axios.create({
-  baseURL: process.env.GRADUATION_API_URL,
+export const departmentsAPI = axios.create({
+  baseURL: process.env.DEPARTMENTS_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const instructorTaAPI = axios.create({
+  baseURL: process.env.INSTRUCTOR_TA_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const departmentEnrollmentAPI = axios.create({
+  baseURL: process.env.DEPARTMENT_ENROLLMENT_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const semesterAPI = axios.create({
+  baseURL: process.env.SEMESTER_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const hallSlotAPI = axios.create({
+  baseURL: process.env.HALL_SLOT_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const scheduleAPI = axios.create({
+  baseURL: process.env.SCHEDULE_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const bylawAPI = axios.create({
+  baseURL: process.env.BYLAWS_API_URL,
+  validateStatus: (status) => status < 500,
+});
+
+export const configAPI = axios.create({
+  baseURL: process.env.CONFIG_API_URL,
   validateStatus: (status) => status < 500,
 });

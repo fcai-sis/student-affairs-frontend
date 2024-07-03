b11b704792e918ca0f4544ea4aaa30ac3234dce9
Amr Khaled <lewolfje@gmail.com>
2024-07-03 17:25:23 +0300

    add level filter

--- app/[locale]/(authenticated)/students/page.tsx
+++ app/[locale]/(authenticated)/students/page.tsx
@@ -6,6 +6,7 @@ import {
   TextFilter,
 } from "@/components/SetQueryFilter";
 import StudentCard from "@/components/StudentCard";
+import { localizedLevel } from "@/dummy/utils";
 import { getAccessToken, getCurrentPage, limit, tt } from "@/lib";
 import { getCurrentLocale, getI18n } from "@/locales/server";
 import { GenderEnum } from "@fcai-sis/shared-models";
@@ -78,6 +79,29 @@ export default async function Page({
     },
   ];
 
+  const levelOptions: SelectOption[] = [
+    {
+      label: tt(locale, { en: "All", ar: "الكل" }),
+      value: "",
+    },
+    {
+      label: tt(locale, localizedLevel(1)),
+      value: "1",
+    },
+    {
+      label: tt(locale, localizedLevel(2)),
+      value: "2",
+    },
+    {
+      label: tt(locale, localizedLevel(3)),
+      value: "3",
+    },
+    {
+      label: tt(locale, localizedLevel(4)),
+      value: "4",
+    },
+  ];
+
   return (
     <>
       <h1 className='text-3xl font-bold mb-4'>{t("students.title")}</h1>
@@ -92,15 +116,19 @@ export default async function Page({
       <div className='flex flex-col gap-2 mt-4'>
         <div className='flex gap-4'>
           <label className='flex flex-col'>
-            {t("filter.department")}:
+            {t("filter.department")}
             <SelectFilter name={"department"} options={departmentOptions} />
           </label>
           <label className='flex flex-col'>
-            {t("filter.search")}:
+            {t("filter.search")}
             <TextFilter name={"query"} />
           </label>
           <label className='flex flex-col'>
-            {t("filter.gender")}:
+            {t("filter.level")}
+            <SelectFilter name={"level"} options={levelOptions} />
+          </label>
+          <label className='flex flex-col'>
+            {t("filter.gender")}
             <SelectFilter name={"gender"} options={genderOptions} />
           </label>
         </div>
